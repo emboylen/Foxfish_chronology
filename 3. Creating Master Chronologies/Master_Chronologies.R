a8fb4742452c96e2379b6f79e011b64cd3690e7c
Ellen <emboylen@gmail.com>
2014-07-01 14:19:20 +0800

    Updating Master Chronologies

---
+++ 3. Creating Master Chronologies/Master_Chronologies.R
@@ -0,0 +1,55 @@
+##########################
+### Increment Analysis ###
+##########################
+
+##################
+### Initialize ###
+##################
+
+setwd("C:/Users/User/Google Drive/Honours/Results/IncrementData") # Ellen's pc
+#setwd("C:/Users/Ellen/Google Drive/Honours/Results/IncrementData") # laptop
+# clear environment
+rm(list=ls(all=TRUE))
+# Read in csv files
+list.files()
+SFoxPC <- read.csv("SouthFoxPC.csv")
+WFoxPC <- read.csv("WestFoxPC.csv")
+
+
+# Creating master chronologies using dplR
+# remove measurements with < 5 observations
+MinN <- 5
+SFoxPC1 <- SFoxPC[,-1]
+NS <- ncol(SFoxPC1)
+a <- NS-MinN
+WFoxPC1 <- WFoxPC[,-1]
+NW <- ncol(WFoxPC1)
+b <- NW-MinN
+
+
+library(dplR)
+dat1 <- chron(x=SFoxPC1, prefix = "xxx", biweight = TRUE, prewhiten = FALSE)
+dat1 <- data.frame(SFoxPC[,1], dat1)
+dat2 <- dat1[dat1$samp.depth >= a,]
+dat3 <- chron(x=WFoxPC1, prefix = "xxx", biweight = TRUE, prewhiten = FALSE)
+dat3 <- data.frame(WFoxPC[,1], dat3)
+dat4 <- dat1[dat1$samp.depth >= b,]
+
+# write data to csv
+write.csv(dat2, file = "Master_SouthFOX.csv", row.names=F)
+write.csv(dat4, file = "Master_WestFOX.csv", row.namses=F)
+
+
+# Alternatively: creating master chronologies - manually
+# library(reshape2)
+# 
+# SouthFOXMean <- rowMeans(SFoxPC[,-1], na.rm = TRUE, dims = 1)
+# NewSouthFOX <- data.frame(SFoxPC[,1], SouthFOXMean)
+# colnames(NewSouthFOX) <- c("Year", "Mean")
+# WestFOXMean <- rowMeans(WFoxPC[,-1], na.rm = TRUE, dims = 1)
+# NewWestFOX <- data.frame(WFoxPC[,1], WestFOXMean)
+# colnames(NewWestFOX) <- c("Year", "Mean")
+# 
+# # Plotting
+# plot(NewWestFOX, type="l")
+# plot(NewSouthFOX, type="l")
